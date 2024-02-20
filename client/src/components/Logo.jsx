4162b6cbfa979bd34718cc17474a86da7a59519c
Omar Alkhaled <omarkh1997aled@gmail.com>
2024-02-20 14:23:35 +0100

    last changes

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom";
-import logo from "../assets/images/Logo.svg";
-import PropTypes from "prop-types";
-const Logo = ({ width, height, textClasses }) => {
-	return (
-		<div className="flex justify-center items-center gap-2">
-			<img
-				src={logo}
-				style={{ width: `${width}px`, height: `${height}px` }}
-				alt="DrawVerse Logo"
-			/>
-			<Link to={"/"} className={textClasses}>
-				DrawVerse
-			</Link>
-		</div>
-	);
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/Logo.svg';
+
+const Logo = ({ width, height, textClasses, divStyle }) => {
+  console.log(width);
+  return (
+    <div
+      className={`flex justify-center items-center gap-2 ${
+        divStyle && divStyle
+      } `}
+      style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}
+    >
+      <img
+        src={logo}
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+        }}
+        alt='DrawVerse Logo'
+      />
+      <Link to={'/'} className={textClasses}>
+        DrawVerse
+      </Link>
+    </div>
+  );
 };
-Logo.propTypes = {
-	width: PropTypes.number.isRequired,
-	height: PropTypes.number.isRequired,
-	textClasses: PropTypes.string.isRequired,
-};
+
 export default Logo;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,94 +1,94 @@
-import User from "../models/userSchema.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import "dotenv/config";
+import User from '../models/userSchema.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export const userSignUp = async (req, res) => {
-	const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-	const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-	const newUser = new User({
-		username,
-		email,
-		password: hashedPassword,
-	});
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-	console.log(req.body);
+  console.log(req.body);
 
-	try {
-		if (!newUser) {
-			res.status(500).send("all field are required");
-		}
-		await newUser.save();
-		res.status(201).send(`New User: ${username} is added`);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	}
+  try {
+    if (!newUser) {
+      res.status(500).send('all field are required');
+    }
+    await newUser.save();
+    res.status(201).send(`New User: ${username} is added`);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const userSignIn = async (req, res) => {
-	const { email, password } = req.body;
+  const { email, password } = req.body;
 
-	try {
-		const loggedUser = await User.findOne({ email: email });
+  try {
+    const loggedUser = await User.findOne({ email: email });
 
-		if (!loggedUser) {
-			return res.status(501).send({
-				success: false,
-				error: "User or Password are not correct",
-			});
-		}
+    if (!loggedUser) {
+      return res.send({
+        success: false,
+        error: 'User or Password are not correct',
+      });
+    }
 
-		const comparedPassword = await bcrypt.compare(
-			password,
-			loggedUser.password
-		);
-		if (!comparedPassword) {
-			return res.status(501).send({
-				success: false,
-				error: "User or Password are not correct",
-			});
-		}
-		const expiresInMs = 24 * 60 * 60 * 1000;
+    const comparedPassword = await bcrypt.compare(
+      password,
+      loggedUser.password
+    );
+    if (!comparedPassword) {
+      return res.send({
+        success: false,
+        error: 'User or Password are not correct',
+      });
+    }
+    const expiresInMs = 24 * 60 * 60 * 1000;
 
-		const expiresInDate = new Date(Date.now() + expiresInMs);
+    const expiresInDate = new Date(Date.now() + expiresInMs);
 
-		const token = jwt.sign({ userId: loggedUser._id }, process.env.JWT_SECRET, {
-			expiresIn: expiresInMs,
-		});
+    const token = jwt.sign({ userId: loggedUser._id }, process.env.JWT_SECRET, {
+      expiresIn: expiresInMs,
+    });
 
-		const cookieOptions = {
-			httpOnly: true,
-			maxAge: expiresInMs,
-		};
+    const cookieOptions = {
+      httpOnly: true,
+      maxAge: expiresInMs,
+    };
 
-		res.cookie("jwt", token, cookieOptions);
+    res.cookie('jwt', token, cookieOptions);
 
-		const options = {
-			maxAge: expiresInMs,
-		};
-		const payload = {
-			expires: expiresInDate.toISOString(),
-			userId: loggedUser._id,
-		};
-		res.cookie("JWTinfo", payload, options);
-		return res.send({
-			success: true,
-			msg: "User logged in",
-			userId: loggedUser._id,
-		});
-	} catch (error) {
-		res.status(500).send({
-			success: false,
-			error: error.message,
-		});
-	}
+    const options = {
+      maxAge: expiresInMs,
+    };
+    const payload = {
+      expires: expiresInDate.toISOString(),
+      userId: loggedUser._id,
+    };
+    res.cookie('JWTinfo', payload, options);
+    return res.send({
+      success: true,
+      msg: 'User logged in',
+      userId: loggedUser._id,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 // Logout
 export const Logout = async (req, res) => {
-	res.clearCookie("jwt");
-	res.clearCookie("JWTinfo");
-	res.send({ msg: "successfully logged out" });
+  res.clearCookie('jwt');
+  res.clearCookie('JWTinfo');
+  res.send({ msg: 'successfully logged out' });
 };
