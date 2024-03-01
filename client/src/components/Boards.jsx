0165e66bb6ebfd8038f12047796a2ed503254e9c
Omar Alkhaled <omarkh1997aled@gmail.com>
2024-03-01 15:21:36 +0100

    error fixed

--- client/src/components/Boards.jsx
+++ client/src/components/Boards.jsx
@@ -1,157 +1,162 @@
-import { HiOutlineDotsVertical } from "react-icons/hi";
-import EditBoard from "./EditBoard";
-import React, { useContext, useEffect, useState } from "react";
-import { FaPlus } from "react-icons/fa6";
-import { UserContext } from "../context/UserContext";
-import axios from "axios";
-import EmptyBoards from "./EmptyBoards";
-import { Link, useNavigate } from "react-router-dom";
-import DeleteBoard from "./DeleteBoard";
+import { HiOutlineDotsVertical } from 'react-icons/hi';
+import EditBoard from './EditBoard';
+import React, { useContext, useEffect, useState } from 'react';
+import { FaPlus } from 'react-icons/fa6';
+import { UserContext } from '../context/UserContext';
+import axios from 'axios';
+import EmptyBoards from './EmptyBoards';
+import { Link, useNavigate } from 'react-router-dom';
+import DeleteBoard from './DeleteBoard';
 
 const Boards = () => {
-	const { userId, backendApiUrl, handleCreateBoard, boards, setBoards } =
-		useContext(UserContext);
-	const [editBoardData, setEditBoardData] = useState({ id: "", name: "" });
-	const [isEditBoard, setIsEditBoard] = useState(false);
-	const [isDeleteBoard, setIsDeleteBoard] = useState(false);
-	const [isBoard, seIsBoard] = useState(false);
+  const { userId, backendApiUrl, handleCreateBoard, boards, setBoards } =
+    useContext(UserContext);
+  const [editBoardData, setEditBoardData] = useState({ id: '', name: '' });
+  const [isEditBoard, setIsEditBoard] = useState(false);
+  const [isDeleteBoard, setIsDeleteBoard] = useState(false);
+  const [isBoard, seIsBoard] = useState(false);
 
-	const navigate = useNavigate();
-	const toggleEditBoard = (boardId) => {
-		setEditBoardData((prevData) => ({
-			...prevData,
-			id: boardId === editBoardData.id ? null : boardId,
-		}));
-	};
+  const navigate = useNavigate();
+  const toggleEditBoard = (boardId) => {
+    setEditBoardData((prevData) => ({
+      ...prevData,
+      id: boardId === editBoardData.id ? null : boardId,
+    }));
+  };
 
-	useEffect(() => {
-		axios
-			.post(
-				`${backendApiUrl}/getBoards`,
-				{ userId: userId },
-				{
-					withCredentials: true,
-				}
-			)
-			.then((res) => {
-				setBoards(res.data.boards);
-				if (res.data.boards.length) {
-					seIsBoard(false);
-				} else {
-					seIsBoard(true);
-				}
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, [isDeleteBoard, isEditBoard]);
+  useEffect(() => {
+    axios
+      .post(
+        `${backendApiUrl}/getBoards`,
+        { userId: userId },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        setBoards(res.data.boards);
+        if (res.data.boards.length >= 1) {
+          console.log('response', res.data);
+          seIsBoard(false);
+        } else {
+          setBoards([]);
+          seIsBoard(true);
+        }
+      })
+      .catch((err) => {
+        console.log('-------------------------', err);
+        setBoards([]);
+        seIsBoard(true);
+      });
+  }, [isDeleteBoard, isEditBoard]);
 
-	// todo: this function should be in separate file
-	const updateBoard = () => {
-		const newBoard = handleCreateBoard();
+  // todo: this function should be in separate file
+  const updateBoard = () => {
+    const newBoard = handleCreateBoard();
 
-		setBoards([...boards, newBoard]);
-	};
+    setBoards([...boards, newBoard]);
+  };
 
-	if (isBoard) {
-		return <EmptyBoards seIsBoard={seIsBoard} />;
-	}
+  if (isBoard) {
+    return <EmptyBoards seIsBoard={seIsBoard} />;
+  }
+  console.log('isBoard', isBoard);
 
-	return (
-		<div className="grid grid-cols-1 px-16 my-10 gap-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-			{boards.length ? (
-				<div
-					className="relative p-20 sm:p-0 max-h-[240px] rounded-md cursor-pointer flex flex-col justify-center items-center bg-black transition-colors group"
-					onClick={updateBoard}
-				>
-					<span className="absolute w-0 h-0 transition-all duration-200 ease-linear bg-yellow-500 rounded-full group-hover:rounded-md group-hover:w-full group-hover:h-full"></span>
-					<span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-					<FaPlus className="text-3xl group-hover:rotate-180 transition delay-75 duration-300 group-hover:text-black" />
-					<span className="font-bold text-lg z-10 group-hover:text-black transition delay-75 duration-300">
-						New Board
-					</span>
-				</div>
-			) : null}
+  return (
+    <div className='grid grid-cols-1 px-16 my-10 gap-5 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+      {boards.length >= 1 && (
+        <div
+          className='relative p-20 sm:p-0 max-h-[240px] rounded-md cursor-pointer flex flex-col justify-center items-center bg-black transition-colors group'
+          onClick={updateBoard}
+        >
+          <span className='absolute w-0 h-0 transition-all duration-200 ease-linear bg-yellow-500 rounded-full group-hover:rounded-md group-hover:w-full group-hover:h-full'></span>
+          <span className='absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700'></span>
+          <FaPlus className='text-3xl group-hover:rotate-180 transition delay-75 duration-300 group-hover:text-black' />
+          <span className='font-bold text-lg z-10 group-hover:text-black transition delay-75 duration-300'>
+            New Board
+          </span>
+        </div>
+      )}
 
-			{boards &&
-				boards.map((board) => {
-					return (
-						<React.Fragment key={board.boardId}>
-							<div
-								className="group bg-slate-300 cursor-pointer relative rounded-md"
-								onClick={() => navigate(`/whiteboard/${board.boardId}`)}
-							>
-								<img
-									src={board.imageUrl}
-									alt="board-image"
-									className="w-[300px] h-[200px] object-fill rounded-t-md"
-								/>
+      {boards.length >= 1 &&
+        boards.map((board) => {
+          return (
+            <React.Fragment key={board.boardId}>
+              <div
+                className='group bg-slate-300 cursor-pointer relative rounded-md'
+                onClick={() => navigate(`/whiteboard/${board.boardId}`)}
+              >
+                <img
+                  src={board.imageUrl}
+                  alt='board-image'
+                  className='w-[300px] h-[200px] object-fill rounded-t-md'
+                />
 
-								<p className="p-2 text-black font-medium">
-									{board.name.length > 15
-										? `${board.name.slice(0, 15)}...`
-										: board.name[0].toLocaleUpperCase() + board.name.slice(1)}
-								</p>
+                <p className='p-2 text-black font-medium'>
+                  {board.name.length > 15
+                    ? `${board.name.slice(0, 15)}...`
+                    : board.name[0].toLocaleUpperCase() + board.name.slice(1)}
+                </p>
 
-								<HiOutlineDotsVertical
-									className="hidden group-hover:block absolute top-2 right-2 z-10 text-xl bg-black rounded-full w-6 h-6 p-1"
-									onClick={(e) => {
-										e.stopPropagation();
-										toggleEditBoard(board.boardId);
-									}}
-								/>
+                <HiOutlineDotsVertical
+                  className='hidden group-hover:block absolute top-2 right-2 z-10 text-xl bg-black rounded-full w-6 h-6 p-1'
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    toggleEditBoard(board.boardId);
+                  }}
+                />
 
-								{editBoardData.id === board.boardId && (
-									<ul className="hidden group-hover:block h-fit p-2 text-xs z-10 bg-gray-800 rounded-md absolute top-8 right-4">
-										<li
-											className="cursor-pointer hover:bg-slate-800 p-1 border-gray-800 border-b hover:border-[#DFB700] delay-75 duration-150"
-											onClick={(e) => {
-												e.stopPropagation();
-												setIsEditBoard(true);
-												setEditBoardData({
-													id: board.boardId,
-													name: board.name,
-												});
-											}}
-										>
-											Change Name
-										</li>
-										<li
-											className="cursor-pointer hover:bg-slate-800 p-1 border-gray-800 border-b hover:border-[#DFB700] delay-75 duration-150"
-											onClick={(e) => {
-												e.stopPropagation();
-												setIsDeleteBoard(true);
-												setEditBoardData({
-													id: board.boardId,
-													name: board.name,
-												});
-											}}
-										>
-											Delete Board
-										</li>
-									</ul>
-								)}
+                {editBoardData.id === board.boardId && (
+                  <ul className='hidden group-hover:block h-fit p-2 text-xs z-10 bg-gray-800 rounded-md absolute top-8 right-4'>
+                    <li
+                      className='cursor-pointer hover:bg-slate-800 p-1 border-gray-800 border-b hover:border-[#DFB700] delay-75 duration-150'
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setIsEditBoard(true);
+                        setEditBoardData({
+                          id: board.boardId,
+                          name: board.name,
+                        });
+                      }}
+                    >
+                      Change Name
+                    </li>
+                    <li
+                      className='cursor-pointer hover:bg-slate-800 p-1 border-gray-800 border-b hover:border-[#DFB700] delay-75 duration-150'
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setIsDeleteBoard(true);
+                        setEditBoardData({
+                          id: board.boardId,
+                          name: board.name,
+                        });
+                      }}
+                    >
+                      Delete Board
+                    </li>
+                  </ul>
+                )}
 
-								<div className="absolute inset-0 h-full w-full opacity-0 group-hover:opacity-50 transition-opacity bg-black" />
-							</div>
+                <div className='absolute inset-0 h-full w-full opacity-0 group-hover:opacity-50 transition-opacity bg-black' />
+              </div>
 
-							{isEditBoard && (
-								<EditBoard
-									setIsEditBoard={setIsEditBoard}
-									boardData={editBoardData}
-								/>
-							)}
+              {isEditBoard && (
+                <EditBoard
+                  setIsEditBoard={setIsEditBoard}
+                  boardData={editBoardData}
+                />
+              )}
 
-							{isDeleteBoard && (
-								<DeleteBoard
-									setIsDeleteBoard={setIsDeleteBoard}
-									boardData={editBoardData}
-								/>
-							)}
-						</React.Fragment>
-					);
-				})}
-		</div>
-	);
+              {isDeleteBoard && (
+                <DeleteBoard
+                  setIsDeleteBoard={setIsDeleteBoard}
+                  boardData={editBoardData}
+                />
+              )}
+            </React.Fragment>
+          );
+        })}
+    </div>
+  );
 };
 export default Boards;
--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,116 +1,115 @@
-import { createContext, useEffect, useState } from "react";
-import cookie from "js-cookie";
-import { getRandomImage } from "../components/liveBlocks/Images";
-import { v4 as randomId } from "uuid";
-import axios from "axios";
+import { createContext, useEffect, useState } from 'react';
+import cookie from 'js-cookie';
+import { getRandomImage } from '../components/liveBlocks/Images';
+import { v4 as randomId } from 'uuid';
+import axios from 'axios';
 export const UserContext = createContext({});
 export const UserContextProvider = ({ children }) => {
-	const backendApiUrl =
-		import.meta.env.VITE_NODE_ENV === "development"
-			? "http://localhost:3000"
-			: "vercel";
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const [userId, setUserId] = useState("");
-	const [boards, setBoards] = useState([]);
-	const [userData, setUserData] = useState({
-		userName: "",
-		email: "",
-		password: "",
-		imageUrl: "",
-	});
-	const [currentUser, setCurrentUser] = useState({
-		username: "",
-		email: "",
-		image: "",
-		password: "",
-	});
-	const handleIfUserHasToken = async () => {
-		let JWTInfoCookie = cookie.get("JWTinfo");
-		if (!JWTInfoCookie) return false;
-		JWTInfoCookie = JWTInfoCookie.replace("j:", "");
-		const cookieValueObj = JSON.parse(JWTInfoCookie);
-		const expirationInMs = new Date(cookieValueObj.expires) - new Date();
-		const getUserId = cookieValueObj.userId;
-		setUserId(getUserId);
-		if (expirationInMs <= 0) return null;
-		return JWTInfoCookie;
-	};
-	const checkIfIsAuthenticated = async () => {
-		const token = handleIfUserHasToken();
-		if (token) {
-			const res = await axios.post(
-				`${backendApiUrl}/isAuth`,
-				{ token },
-				{ withCredentials: true }
-			);
-			if (res.data.isAuth) {
-				setIsAuthenticated(true);
-				return true;
-			} else {
-				setIsAuthenticated(false);
-				return false;
-			}
-		} else {
-			return false;
-		}
-	};
-	// TODO: handleCreateBoard in separate file;
-	const handleCreateBoard = () => {
-		let newBoard = {
-			userId: userId,
-			boardId: randomId(),
-			name: "untitled",
-			imageUrl: getRandomImage(),
-		};
-		axios.post(`${backendApiUrl}/createBoard`, {
-			newBoard,
-		});
-		return newBoard;
-	};
-	useEffect(() => {
-		// wird dreimal ausgeführt TODO: Checken!!!!!!!!!
-		const getUser = async () => {
-			if (!userId) {
-				// console.warn(test);
-				console.error("userId not set");
-				return;
-			}
-			try {
-				const res = await axios.get(`${backendApiUrl}/getUser/${userId}`);
-				const imageFromRes = res.data.imageUrl;
-				const dataUrl = `data:image/png;base64,${imageFromRes}`;
-				setCurrentUser({
-					username: res.data.username,
-					email: res.data.email,
-					image: !imageFromRes
-						? dataUrl.replace(/^data:image\/[a-z]+;base64,/, "")
-						: dataUrl,
-				});
-				console.log(res.data);
-			} catch (error) {
-				console.error("Error fetching user data:", error);
-			}
-		};
-		getUser();
-	}, [userId, currentUser.image]);
-	return (
-		<UserContext.Provider
-			value={{
-				backendApiUrl,
-				isAuthenticated,
-				checkIfIsAuthenticated,
-				setIsAuthenticated,
-				userId,
-				handleCreateBoard,
-				boards,
-				setBoards,
-				setUserData,
-				userData,
-				setCurrentUser,
-				currentUser,
-			}}
-		>
-			{children}
-		</UserContext.Provider>
-	);
+  const backendApiUrl =
+    import.meta.env.VITE_NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'vercel';
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [boards, setBoards] = useState([]);
+  const [userData, setUserData] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    imageUrl: '',
+  });
+  const [currentUser, setCurrentUser] = useState({
+    username: '',
+    email: '',
+    image: '',
+    password: '',
+  });
+
+  const handleIfUserHasToken = () => {
+    let JWTInfoCookie = cookie.get('JWTinfo');
+    if (!JWTInfoCookie) return false;
+    JWTInfoCookie = JWTInfoCookie.replace('j:', '');
+    const cookieValueObj = JSON.parse(JWTInfoCookie);
+    const expirationInMs = new Date(cookieValueObj.expires) - new Date();
+    const getUserId = cookieValueObj.userId;
+    setUserId(getUserId);
+    if (expirationInMs <= 0) return null;
+    return JWTInfoCookie;
+  };
+
+  const checkIfIsAuthenticated = async () => {
+    const token = handleIfUserHasToken();
+    if (token) {
+      const res = await axios.post(
+        `${backendApiUrl}/isAuth`,
+        { token },
+        { withCredentials: true }
+      );
+      if (res.data.isAuth) {
+        setIsAuthenticated(true);
+        return true;
+      } else {
+        setIsAuthenticated(false);
+        return false;
+      }
+    } else {
+      return false;
+    }
+  };
+  // TODO: handleCreateBoard in separate file;
+  const handleCreateBoard = () => {
+    let newBoard = {
+      userId: userId,
+      boardId: randomId(),
+      name: 'untitled',
+      imageUrl: getRandomImage(),
+    };
+    axios.post(`${backendApiUrl}/createBoard`, {
+      newBoard,
+    });
+    return newBoard;
+  };
+  useEffect(() => {
+    // wird dreimal ausgeführt TODO: Checken!!!!!!!!!
+    const getUser = async () => {
+      try {
+        const res = await axios.get(`${backendApiUrl}/getUser/${userId}`);
+        const imageFromRes = res.data.imageUrl;
+        const dataUrl = `data:image/png;base64,${imageFromRes}`;
+
+        setCurrentUser({
+          username: res.data.username,
+          email: res.data.email,
+          image: !imageFromRes
+            ? dataUrl.replace(/^data:image\/[a-z]+;base64,/, '')
+            : dataUrl,
+        });
+        console.log(res.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+    getUser();
+  }, [userId, currentUser.image]);
+
+  return (
+    <UserContext.Provider
+      value={{
+        backendApiUrl,
+        isAuthenticated,
+        checkIfIsAuthenticated,
+        setIsAuthenticated,
+        userId,
+        handleCreateBoard,
+        boards,
+        setBoards,
+        setUserData,
+        userData,
+        setCurrentUser,
+        currentUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
