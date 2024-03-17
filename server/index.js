d0065179bc2dd9ea739cc374f5c50b0dcef51cd9
Omar Alkhaled <omarkh1997aled@gmail.com>
2024-03-17 20:44:18 +0100

    vercel

--- server/index.js
+++ server/index.js
@@ -20,14 +20,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 });
 
 app.use(
   cors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     credentials: true,
   })
 );
