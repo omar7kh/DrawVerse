f3e09822132c5e405482ca242e492c9c1c4f3b44
Omar Alkhaled <omarkh1997aled@gmail.com>
2024-03-27 00:47:40 +0100

    change orgin Url

--- server/index.js
+++ server/index.js
@@ -19,18 +19,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new socketIo(server, {
   cors: {
-    origin: process.env.NODE_ENV
-      ? process.env.CLIENT_URL
-      : process.env.CLIENT_DOMAIN,
+    origin: process.env.CLIENT_DOMAIN,
     methods: ['GET', 'POST'],
   },
 });
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV
-      ? process.env.CLIENT_URL
-      : process.env.CLIENT_DOMAIN,
+    origin: process.env.CLIENT_DOMAIN,
     credentials: true,
   })
 );
