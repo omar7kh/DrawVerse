204f89b848b3de2b34d35c814ad0302fbcedb825
Uftadi <akif.gueler@dci.education>
2024-03-26 16:34:31 +0100

    nfp

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,105 +1,109 @@
-import { UserContext } from '../context/UserContext';
-import { useNavigate } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
-import { Boards, DropdownMenu, Logo, NotificationsBox } from '../components';
-import avatar from '../assets/images/avatar.png';
-import '../index.css';
-import { IoMdNotifications } from 'react-icons/io';
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Boards, DropdownMenu, Logo, NotificationsBox } from "../components";
+import avatar from "../assets/images/avatar.png";
+import "../index.css";
+import { IoMdNotifications } from "react-icons/io";
 
 const MainPage = () => {
-  const {
-    checkIfIsAuthenticated,
-    isAuthenticated,
-    setIsAuthenticated,
-    currentUser,
-    notifications,
-    setMembers,
-  } = useContext(UserContext);
-  const navigate = useNavigate();
+	const {
+		checkIfIsAuthenticated,
+		isAuthenticated,
+		setIsAuthenticated,
+		currentUser,
+		notifications,
+		setMembers,
+	} = useContext(UserContext);
+	const navigate = useNavigate();
 
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [shake, setShake] = useState(false);
-  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
-  const [notificationsNumber, setNotificationsNumber] = useState(0);
+	const [showDropdown, setShowDropdown] = useState(false);
+	const [shake, setShake] = useState(false);
+	const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+	const [notificationsNumber, setNotificationsNumber] = useState(0);
 
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
+	const toggleDropdown = () => {
+		setShowDropdown(!showDropdown);
+	};
 
-  useEffect(() => {
-    setMembers([]);
-    const check = async () => {
-      const checkIsAuth = await checkIfIsAuthenticated();
-      if (checkIsAuth) {
-        setIsAuthenticated(true);
-      } else {
-        navigate('/login');
-        setIsAuthenticated(false);
-        console.log('not authenticated');
-      }
-    };
+	useEffect(() => {
+		setMembers([]);
+		const check = async () => {
+			const checkIsAuth = await checkIfIsAuthenticated();
+			if (checkIsAuth) {
+				setIsAuthenticated(true);
+			} else {
+				navigate("/login");
+				setIsAuthenticated(false);
+				console.log("not authenticated");
+			}
+		};
 
-    check();
-  }, [isAuthenticated]);
+		check();
+	}, [isAuthenticated]);
 
-  useEffect(() => {
-    setNotificationsNumber(notifications.length);
-  }, [notifications]);
+	useEffect(() => {
+		setNotificationsNumber(notifications.length);
+	}, [notifications]);
 
-  return (
-    <div className='min-h-full w-full bg-[#1F2937] text-white'>
-      {isAuthenticated && (
-        <>
-          <nav className='flex justify-between bg-yellow-500 items-center px-5 h-14 z-50 lg:px-16'>
-            <Logo
-              width={35}
-              height={35}
-              textClasses='font-bold text-sm md:text-lg lg:text-xl'
-            />
-            <div className='flex justify-center items-center gap-5'>
-              <div className='relative'>
-                <IoMdNotifications
-                  className={`text-2xl text-black cursor-pointer ${
-                    shake ? 'animate-shake' : ''
-                  }`}
-                  onMouseEnter={() => setShake(true)}
-                  onMouseLeave={() => setShake(false)}
-                  onClick={() => setIsNotificationOpen(!isNotificationOpen)}
-                />
-                {notificationsNumber >= 1 && (
-                  <span className='absolute -top-1 right-[-6px] text-xs bg-black rounded-full flex justify-center items-center w-[17px] h-[17px] overflow-hidden'>
-                    {notificationsNumber}
-                  </span>
-                )}
+	return (
+		<div className="min-h-full w-full bg-[#1F2937] text-white">
+			{isAuthenticated && (
+				<>
+					<nav className="flex justify-between bg-yellow-500 items-center px-5 h-14 z-50 lg:px-16">
+						<Logo
+							width={35}
+							height={35}
+							textClasses="font-bold text-sm md:text-lg lg:text-xl"
+						/>
+						<div className="flex justify-center items-center gap-5">
+							<div className="relative">
+								<IoMdNotifications
+									className={`text-2xl text-black cursor-pointer ${
+										shake ? "animate-shake" : ""
+									}`}
+									onMouseEnter={() => setShake(true)}
+									onMouseLeave={() => setShake(false)}
+									onClick={() => setIsNotificationOpen(!isNotificationOpen)}
+								/>
+								{notificationsNumber >= 1 && (
+									<span className="absolute -top-1 right-[-6px] text-xs bg-black rounded-full flex justify-center items-center w-[17px] h-[17px] overflow-hidden">
+										{notificationsNumber}
+									</span>
+								)}
 
-                {isNotificationOpen && (
-                  <NotificationsBox
-                    setIsNotificationOpen={setIsNotificationOpen}
-                  />
-                )}
-              </div>
+								{isNotificationOpen && (
+									<NotificationsBox
+										setIsNotificationOpen={setIsNotificationOpen}
+									/>
+								)}
+							</div>
 
-              <div className='w-[45px] h-[45px] relative'>
-                <img
-                  src={!currentUser.image ? avatar : currentUser.image}
-                  alt='avatar'
-                  className='w-full h-full object-cover rounded-full relative cursor-pointer'
-                  onClick={toggleDropdown}
-                />
-                {showDropdown && (
-                  <div className='absolute top-[45px] right-6 z-10'>
-                    <DropdownMenu />
-                  </div>
-                )}
-              </div>
-            </div>
-          </nav>
-          <div className='h-[calc(100vh-56px)] w-full flex justify-center items-start overflow-x-auto'>
-            <Boards />
-          </div>
-        </>
-      )}
-    </div>
-  );
+							<div className="w-[45px] h-[45px] relative">
+								<img
+									src={
+										currentUser.image == "undefined"
+											? avatar
+											: currentUser.image
+									}
+									alt="avatar"
+									className="w-full h-full object-cover rounded-full relative cursor-pointer"
+									onClick={toggleDropdown}
+								/>
+								{showDropdown && (
+									<div className="absolute top-[45px] right-6 z-10">
+										<DropdownMenu />
+									</div>
+								)}
+							</div>
+						</div>
+					</nav>
+					<div className="h-[calc(100vh-56px)] w-full flex justify-center items-start overflow-x-auto">
+						<Boards />
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
 export default MainPage;
--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,5 +1,10 @@
+import nft from "../assets/images/nfp.jpg";
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+	return (
+		<div className="h-4/6 w-4/6  mx-auto pt-20">
+			<img src={nft} className="w-screen h-[500px]" alt="not-found-image" />
+		</div>
+	);
 };
 
 export default PageNotFound;
